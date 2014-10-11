5e68775626b309e690362743eaaa6e80c81c4c5d
xule1991 <eddiexule@gmail.com>
2014-10-11 13:32:37 +0800

    10.11

--- src/algorithm/Fibonacci.java
+++ src/algorithm/Fibonacci.java
@@ -11,13 +11,32 @@ public class Fibonacci {
 		fibonacci[2] = 1;
 		sum += fibonacci[2]; 
 		for (int i = 3; i <= 1000; i++) {
-			if ((i % 2) ==0)
 			fibonacci[i] = fibonacci[i-1] + fibonacci[i-2]; 
+			if ((i % 2) ==0)
 			sum += fibonacci[i];
 		}
 		return sum;
 	}
-	public static void main() {
-		
+	
+	public void printFibonacci() {
+		int first = 1;
+		int second = 1;
+		int third = 0;
+		System.out.print(first + "   ");
+		System.out.print(second + "   ");
+		int count = 0;
+		for (int i = 3; i <= 1000; i++) {
+			third = first + second;
+			first = second;
+			second = third;
+			System.out.print(third + "  ");
+			if ((++count) % 10 == 0 )
+				System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Fibonacci fibonacci = new Fibonacci();
+		fibonacci.printFibonacci();
 	}
 }
